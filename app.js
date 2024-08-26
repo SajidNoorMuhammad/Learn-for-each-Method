f93464232b23754f49c00f8189f3b899c7e32c05
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-08-26 14:54:13 +0500

    practice

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = "For Each </br> for each takes function and two parameters <br>"
+document.write(heading)
+
+const fruits = ["apple", "mango", "banana", "orange"];
+fruits.forEach(function (fruit, index) {
+    document.write(index + 1 + ":" + fruit + "</br>")
+})
